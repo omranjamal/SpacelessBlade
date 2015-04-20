f8a6d42272ac77db59d464dbd95ead8c938dfe08
Omran Jamal <o.jamal97@gmail.com>
2015-04-20 17:38:35 +0600

    Works

---
+++ src/SpacelessBladeProvider.php
@@ -0,0 +1,22 @@
+<?php 
+namespace solidew\SpacelessBlade;
+
+class SpacelessBladeProvider extends Illuminate\Support\ServiceProvider
+{
+    public function boot()
+    {
+        //Register the Starting Tag
+        \Blade::extend(function($view, $compiler) {
+            $pattern = $compiler->createPlainMatcher('spaceless');
+            return preg_replace($pattern, '<?php ob_start() ?>', $view);
+        });
+
+        //Register the Ending Tag
+        \Blade::extend(function($view, $compiler) {
+            $pattern = $compiler->createPlainMatcher('endspaceless');
+            return preg_replace($pattern, "<?php echo preg_replace('>\\s+<', '><', ob_get_clean()) ?>", $view);
+        });
+    }
+
+    public function register(){}
+}
