71bd792b4cc8880751ce620f81126b46405849b4
Omran Jamal <o.jamal97@gmail.com>
2015-04-23 13:49:22 +0600

    Major Fix

--- src/SpacelessBladeProvider.php
+++ src/SpacelessBladeProvider.php
@@ -1,20 +1,21 @@
 <?php 
 namespace solidew\SpacelessBlade;
+use Blade;
 
 class SpacelessBladeProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot()
     {
         //Register the Starting Tag
-        \Blade::extend(function($view, $compiler) {
+        Blade::extend(function($view, $compiler) {
             $pattern = $compiler->createPlainMatcher('spaceless');
             return preg_replace($pattern, '<?php ob_start() ?>', $view);
         });
 
         //Register the Ending Tag
-        \Blade::extend(function($view, $compiler) {
+        Blade::extend(function($view, $compiler) {
             $pattern = $compiler->createPlainMatcher('endspaceless');
-            return preg_replace($pattern, "<?php echo preg_replace('>\\s+<', '><', ob_get_clean()) ?>", $view);
+            return preg_replace($pattern, "<?php echo preg_replace('/>\\s+</', '><', ob_get_clean()); ?>", $view);
         });
     }
 
